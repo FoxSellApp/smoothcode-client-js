0f06c56eb5b69b91c0fe93513d08a2921713d3fa
dev-prakhar <prakhar@pushowl.com>
2022-02-20 00:17:31 +0530

    Added GDPR Webhook Request Verification

--- auth/requests_utils.js
+++ auth/requests_utils.js
@@ -10,5 +10,12 @@ const isWebhookRequest = (requestHmac, clientSecret, webhookData) => {
     return generateHmac(clientSecret, webhookId) === requestHmac
 }
 
+const isGDPRWebhookRequest = (requestHmac, clientSecret, webhookData) => {
+    const shopId = webhookData.shop_id.toString();
+
+    return generateHmac(clientSecret, shopId) === requestHmac
+}
+
 exports.isDashboardRequest = isDashboardRequest
 exports.isWebhookRequest = isWebhookRequest
+exports.isGDPRWebhookRequest = isGDPRWebhookRequest
