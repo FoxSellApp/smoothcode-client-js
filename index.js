6c3ffc4bb8c4f5b9625c4b54aaafa42fc804cd21
dev-prakhar <prakhar@pushowl.com>
2022-02-20 00:23:48 +0530

    Exported GDPR Webhook Request Method

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const {isWebhookRequest} = require("./auth/requests_utils");
 const {isDashboardRequest} = require("./auth/requests_utils");
+const {isGDPRWebhookRequest} = require("./auth/requests_utils");
 
 // Request Utils
 exports.isDashboardRequest = isDashboardRequest
 exports.isWebhookRequest = isWebhookRequest
+exports.isGDPRWebhookRequest = isGDPRWebhookRequest
